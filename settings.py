d90f86807acf9a7d0698c913b723e2d77f3f4d3c
dweagle <dweagle79@gmail.com>
2024-12-12 21:11:08 -0500

    Added Settings Update 3

--- settings.py
+++ settings.py
@@ -2,10 +2,21 @@ import os
 import sys
 import signal
 import logging
+from collections import OrderedDict
 from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import DoubleQuotedScalarString
+from ruamel.yaml.representer import RoundTripRepresenter
 
-# Initialize YAML handler
+# Initialize YAML handler with specific settings
 yaml = YAML()
+yaml.default_flow_style = False
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.allow_unicode = True
+yaml.preserve_quotes = True
+
+# Ensure OrderedDict is used without !!omap tags
+yaml.Representer = RoundTripRepresenter
+yaml.Representer.add_representer(OrderedDict, RoundTripRepresenter.represent_dict)
 
 logger = logging.getLogger(__name__)
 
@@ -17,146 +28,150 @@ def shutdown_gracefully(signal, frame):
 
 signal.signal(signal.SIGTERM, shutdown_gracefully)
 
-# Create default settings yaml file
-settings = f"""# Settings for overlay configurations
-# This containers' scripts will create show status overlay ymls that Kometa can use to create new, airing, ended, canceled and returning overlays.  
-# These overlays files will create dates on the poster overlays. Setting the container schedule to run daily will update airing/return dates.
+# Custom function to add blank lines between sections and their nestings
+def add_blank_lines(yaml_output):
+    lines = yaml_output.split('\n')
+    new_lines = []
+    for i, line in enumerate(lines):
+        new_lines.append(line)
+        # Add a blank line if the next line is a top-level section or nested element
+        if i + 1 < len(lines) and lines[i + 1] and lines[i + 1][0].isalnum() and line and lines[i + 1][0] != '-':
+            new_lines.append('')
+    return '\n'.join(new_lines)
 
-# Mounting this container volumes into your Kometa folder may be the best option.  Then Kometa will automatically have access to the files created.
+# Create default settings
+settings = OrderedDict({
+    "libraries": OrderedDict({
+        "TV Shows": {
+            "is_anime": False,
+            "use_watch_region": True
+        },
+        "4k TV Shows": {
+            "is_anime": False,
+            "use_watch_region": True
+        },
+        "Anime": {
+            "is_anime": True,
+            "use_watch_region": True
+        }
+    }),
 
-# A Returning Soon Collection yaml that can be used with Kometa is also created.  Settings for this are at the very bottom of this settings file.
+    "overlay_settings": OrderedDict({
+        "days_ahead": 28,
+        "overlay_save_folder": None,
+        "font": None,
+        "font_size": 45,
+        "font_color": DoubleQuotedScalarString("#FFFFFF"),
+        "horizontal_align": "center",
+        "vertical_align": "top",
+        "horizontal_offset": 0,
+        "vertical_offset": 38,
+        "back_width": 475,
+        "back_height": 55,
+        "back_radius": 30,
+        "ignore_blank_results": "true",
+        "timezone": "America/New_York",
+        "with_status": 0,
+        "watch_region": "US",
+        "with_original_language": "en",
+        "limit": 500,
+        "with_watch_monetization_types": "flatrate|free|ads|rent|buy"
+    }),
 
-# TMDB_Discover settings pull series info to find air dates, etc.  Using the default settings
-# limits the "junk" show results that are pulled for a library with mainly US, English language shows.  
-# You will get less "No TVDB/TMDB id" errors in Kometa.
+    "use_overlays": OrderedDict({
+        "upcoming_series": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#FC4E03"),
+            "text": DoubleQuotedScalarString("U P C O M I N G"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "new_series": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#008001"),
+            "text": DoubleQuotedScalarString("N E W  S E R I E S"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "new_airing_next": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#008001"),
+            "text": DoubleQuotedScalarString("N E W - A I R S"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "airing_series": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#003880"),
+            "text": DoubleQuotedScalarString("A I R I N G"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "airing_today": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#003880"),
+            "text": DoubleQuotedScalarString("A I R S  T O D A Y"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "airing_next": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#003880"),
+            "text": DoubleQuotedScalarString("A I R I N G"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "ended_series": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#000000"),
+            "text": DoubleQuotedScalarString("E N D E D"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "canceled_series": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#CF142B"),
+            "text": DoubleQuotedScalarString("C A N C E L E D"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "returning_series": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#103197"),
+            "text": DoubleQuotedScalarString("R E T U R N I N G"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        },
+        "returns_next": {
+            "use": True,
+            "back_color": DoubleQuotedScalarString("#103197"),
+            "text": DoubleQuotedScalarString("R E T U R N S"),
+            "font_color": DoubleQuotedScalarString("#FFFFFF")
+        }
+    }),
 
-# If you have an anime library or a TV show library with lots of non English shows, it may be best
-# to NOT use watch_region or with_original_language settings.
-
-libraries:                          # Plex library (SHOWS ONLY) names to create Kometa overlays for.
-  TV Shows:                         # Change, add, or remove - Need at least one library.
-    is_anime: False                 # Removes TMDB with_original_language:'en' setting for use with Anime libraries or libraries with non English shows.         
-    use_watch_region: True          # Removes TMDB watch_region and watch_monetization settings.
-  4k TV Shows:
-    is_anime: False
-    use_watch_region: True
-  Anime:
-    is_anime: True
-    use_watch_region: True
-
-# These settings are used across all status overlays.  
-# This creates a consistent overlay across all shows.    
-overlay_settings:                  
-  days_ahead: 28                            # Days ahead for Returning Next (30 Days Max).
-  overlay_save_folder:                      # Kometa overlay folders (leave blank for config folder). Kometa must have permissions to this folder
-  font:                                     # Kometa must have permissions for this folder. Will default to included font in 'config/fonts/Inter-Medium.ttf'.
-  font_size: 45                             # Font size for overlay text.
-  font_color: "#FFFFFF"                     # Font color (kometa requires #RGB, #RGBA, #RRGGBB or #RRGGBBAA, e.g., #FFFFFF).
-  horizontal_align: center                  # Horizontal alignment (e.g., center, left, right).
-  vertical_align: top                       # Vertical alignment (e.g., top, bottom, etc.).
-  horizontal_offset: 0                      # Horizontal offset in pixels.
-  vertical_offset: 38                       # Vertical offset in pixels.
-  back_width: 475                           # Width of the overlay background.
-  back_height: 55                           # Height of the overlay background.
-  back_radius: 30                           # Corner radius for rounded backgrounds.
-  ignore_blank_results: "true"              # Kometa error processing (true or false).
-
-  # TMDB DISCOVER SETTINGS #                SEE TMDB API FOR MORE DETAILS - THESE SETTINGS ARE IDEAL.
-  timezone: America/New_York                # TMDB DISCOVER - UTC timezone standards (default American/New_York).
-  with_status: 0                            # TMDB DISCOVER - Returning Series: 0 Planned: 1 In Production: 2 Ended: 3 Canceled: 4 Pilot: 5.
-  watch_region: US                          # TMDB DISCOVER - Default US - Must be valid TMDB region code.
-  with_original_language: en                # TMDB DISCOVER - Default is en (English) - Must Be valid TMDB language code.
-  limit: 500                                # TMDB DISCOVER - API Results limit. Default is 500.
-  with_watch_monetization_types: flatrate|free|ads|rent|buy  # TMDB DISCOVER - Options: flaterate, free, ads, rent, buy - can use ,(and) or |(or) as separators.
-
-# You can decide here if you want to use each overlay, change font or backdrop color for individual overlays, or change the text.
-use_overlays:
-  upcoming_series:
-    use: True                               # Use this overlay: True or False.
-    back_color: "#FC4E03"                   # Default is "#fc4e03" - Overlay color override for this overlay only.
-    text: "U P C O M I N G"                 # Change to desired spacing/text.
-    font_color: "#FFFFFF"                   # font color override for this overlay only (Kometa requires #RGB, #RGBA, #RRGGBB or #RRGGBBAA).
-    
-  new_series:
-    use: True
-    back_color: "#008001"                   # Default is "#008001".
-    text: "N E W  S E R I E S"
-    font_color: "#FFFFFF"
-
-  new_airing_next:
-    use: True
-    back_color: "#008001"                   # Default is "#008001".
-    text: "N E W - A I R S"                 # Displays as N E W - A I R S 12/22 on overlays.
-    font_color: "#FFFFFF"
-
-  airing_series:
-    use: True
-    back_color: "#003880"                   # Default is "#003880".
-    text: "A I R I N G"
-    font_color: "#FFFFFF"
-
-  airing_today:
-    use: True
-    back_color: "#003880"                   # Default is "#003880".
-    text: "A I R S  T O D A Y"
-    font_color: "#FFFFFF"
-
-  airing_next:
-    use: True
-    back_color: "#003880"                   # Default is "#003880".
-    text: "A I R I N G "                    # Displays as A I R I N G  12/23 on overlays.
-    font_color: "#FFFFFF"
-
-  ended_series:
-    use: True
-    back_color: "#000000"                   # Default is "#000000.
-    text: "E N D E D"
-    font_color: "#FFFFFF"
-
-  canceled_series:
-    use: True
-    back_color: "#CF142B"                   # Default is "#CF1428".
-    text: "C A N C E L E D"
-    font_color: "#FFFFFF"
-
-  returning_series:
-    use: True
-    back_color: "#103197"                   # Default is "#103197".
-    text: "R E T U R N I N G"
-    font_color: "#FFFFFF" 
-
-  returns_next:
-    use: True
-    back_color: "#103197"                   # Default is "#103197"
-    text: "R E T U R N S "                  # Displays as R E T U R N S  12/23 on overlays.
-    font_color: "#FFFFFF"     
-
-# Creates a Returning Soon collection yml file that can be used in Kometa to display a collection in Plex
-returning_soon_collection:
-  use: True                                 # True to create collection yml. False to not create.
-  collection_save_folder:                   # Path to collection yml folder (leave blank for /config folder). Kometa-must have permissions to this folder.
-  poster_source: url                        # url or file.  url for outside source, file for local poster. Defaults to url
-  poster_path:                              # Path to poster file.  Can be a url link or file path. Defaults to url Kometa git html poster.
-  visible_home: "true"                      # Collecition visible on home page.  "true" or "false"
-  visible_shared: "true"                    # Collection visible on friends/users home page. "true" or "false"
-"""
+    "returning_soon_collection": OrderedDict({
+        "use": True,
+        "collection_save_folder": None,
+        "poster_source": "url",
+        "poster_path": None,
+        "visible_home": "true",
+        "visible_shared": "true"
+    })
+})
 
 def create_settings_file(main_directory):
-
     # Create the settings.yaml file in the same directory as the script.
     settings_file_path = os.path.join(main_directory, settings_filename)
     
     try:
         with open(settings_file_path, 'w') as file:
-            file.write(settings)
+            yaml.dump(settings, file)
+        # Reopen the file to add blank lines between sections
+        with open(settings_file_path, 'r+') as file:
+            yaml_output = file.read()
+            formatted_output = add_blank_lines(yaml_output)
+            file.seek(0)
+            file.write(formatted_output)
+            file.truncate()
         logger.info(f"Created settings file at '{settings_file_path}'")
     
     except Exception as e:
         logger.error(f"Error creating settings file: {e}")
 
 def load_settings(main_directory, log_message=True):
-
-    #Load the settings.yaml file. If it doesn't exist, create it.
+    # Load the settings.yaml file. If it doesn't exist, create it.
     settings_file_path = os.path.join(main_directory, settings_filename)
 
     if not os.path.exists(settings_file_path):
@@ -174,13 +189,28 @@ def load_settings(main_directory, log_message=True):
         raise  # Re-raise exception after logging
 
 def update_dict(existing, defaults):
+    updated = OrderedDict()
+
     for key, value in defaults.items():
-        if isinstance(value, dict):
-            existing[key] = update_dict(existing.get(key, {}), value)
+        if key == "libraries":  # Skip updating libraries section
+            if key in existing:
+                updated[key] = existing[key]
+            continue
+
+        if key in existing:
+            if isinstance(value, dict):
+                updated[key] = update_dict(existing[key], value)
+            else:
+                updated[key] = existing[key]
         else:
-            if key not in existing:
-                existing[key] = value
-    return existing
+            updated[key] = value
+
+    # Add any leftover keys from existing that are not in defaults
+    for key, value in existing.items():
+        if key not in updated:
+            updated[key] = value
+
+    return updated
 
 def update_settings_file(main_directory):
     settings_file_path = os.path.join(main_directory, settings_filename)
@@ -188,12 +218,19 @@ def update_settings_file(main_directory):
     try:
         existing_settings = load_settings(main_directory, log_message=False)
         
-        # Update existing settings with default settings
-        updated_settings = update_dict(existing_settings, yaml.load(settings))
+        # Update existing settings with default settings, but skip libraries section
+        updated_settings = update_dict(existing_settings, settings)
         
         # Save the updated settings back to the file
         with open(settings_file_path, 'w') as file:
             yaml.dump(updated_settings, file)
+        # Reopen the file to add blank lines between sections
+        with open(settings_file_path, 'r+') as file:
+            yaml_output = file.read()
+            formatted_output = add_blank_lines(yaml_output)
+            file.seek(0)
+            file.write(formatted_output)
+            file.truncate()
         logger.info(f"Updated settings file at '{settings_file_path}' with missing sections")
     
     except Exception as e:
